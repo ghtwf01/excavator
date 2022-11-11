573edf8b3679b9ddd4eb63721bc923a8229861c0
liufanrui <liufanrui@bytedance.com>
2022-11-11 20:53:49 +0800

    update

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-EXCLUDES = ["google", "lastpass", '.gov.', 'gvt2.com']
+EXCLUDES = ["lastpass", '.gov.']
 REVERSE_HTTP_IP = "127.0.0.1"  # 回连http IP地址，需要改为服务器ip，不能改为0.0.0.0，因为程序无法识别
 REVERSE_HTTP_PORT = 9999  # 回连http端口
 # REVERSE_RMI_IP = "192.168.0.100"  # Java RMI 回连IP,需要改为服务器ip，不能改为0.0.0.0，因为程序无法识别
